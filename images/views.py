ae6da9e759ae67ebe6c86e594056f05fcd7da1a4
Cyci25 <c2muriithi@gmail.com>
2018-12-11 17:48:21 +0300

    Added new photos to the image.html

--- images/views.py
+++ images/views.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'index.html')
+    return render(request, 'image.html')
 
 def image(request, id):
     try:
@@ -15,5 +15,18 @@ def image(request, id):
         raise Http404()
 
     return render(request, 'images.html', {"image": image})
+
+def search_results(request):
+    
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"picture": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
 
 
