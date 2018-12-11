70d0b42defcf8400164d580b77075d2be396890e
Cyci25 <c2muriithi@gmail.com>
2018-12-11 15:44:33 +0300

    Link the templates

--- images/views.py
+++ images/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
 
+
 # Create your views here.
+# def welcome(request):
+#     return HttpResponse('image.html')
 def welcome(request):
-    return HttpResponse('welcome')
+    return render(request, 'images.html')
+
+# def news_of_day(request):
+#     date = dt.date.today()
+#     # html = f'''
+#     #     <html>
+#     #         <body>
+#     #             <h1> {date.day}-{date.month}-{date.year}</h1>
+#     #         </body>
+#     #     </html>
+#     #         '''
+#     return render(request, 'images.html')
 
 
