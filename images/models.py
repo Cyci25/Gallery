c887aa678fb7b68830b4d45a5bd19e6dfe7806ac
Cyci25 <c2muriithi@gmail.com>
2018-12-13 10:36:33 +0300

    Add the location model

--- images/models.py
+++ images/models.py
@@ -15,5 +15,17 @@ class Category(models.Model):
     def delete_category(self):
         self.delete()
 
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
 
 
